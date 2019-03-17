ef8430ff7aad2499c798049ba229a17b332cfce7
lemonight58 <45255414+lemonight58@users.noreply.github.com>
2019-03-17 16:21:12 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+       
+        public static void Main()
+        {
+            int[] number = { 1, 3, 4, 5 };
+            int i = 0;
+            for (i = 0; i < 4; i++) 
+            {
+                Console.WriteLine($"{number[i]}");
+            }
+            i = 0;
+            while (i < 4) 
+            {
+                Console.WriteLine($"{number[i]}");
+                i++;
+            }
+            i = 0;
+            do
+            {
+                Console.WriteLine($"{number[i]}");
+                i++;
+            } while (i < 4);
+        }
+}
+}
