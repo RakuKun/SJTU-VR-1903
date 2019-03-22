c24716688b9d35e3ca1be1891678251299be64bc
LUWE1W <554220532@qq.com>
2019-03-22 10:19:40 +0800

    Number2String, Reverse, ReverseOrder

--- CSharp/exercise1/exercise1/Program.cs
+++ CSharp/exercise1/exercise1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace exercise1
 {
@@ -10,6 +7,69 @@ namespace exercise1
     {
         static void Main(string[] args)
         {
+            int n = 13333;
+            string s = Number2String(13333);
+            Console.WriteLine(s);
+
+            string[] strs = {"C#", "COBOL", "Java", "C++", "Visual Basic", "Pascal", "Fortran", "Lisp", "J#"};
+            Reverse(strs);
+            foreach (var str in strs)
+            {
+                Console.WriteLine(str);
+            }
+            
+            int[] nums = {2, 4, 3, 1};
+            Console.WriteLine(ReverseOrder(nums));
+
+        }
+
+        static string Number2String(int i)
+        {
+            int j = 0;
+            char[] str = new char[100];
+            string nums = "";
+            while (i > 0)
+            {
+                str[j] = (char)(i % 10 + '0');
+                i = i / 10;
+                j++;
+            }
+
+            for (int k = j - 1; k >= 0; k--)
+            {
+                nums += str[k];
+            }
+
+            return nums;
+        }
+
+        static void Reverse(string[] strs)
+        {
+            string test = String.Empty;
+            for (int i = 0; i < strs.Length / 2; i++)
+            {
+                test = strs[i];
+                strs[i] = strs[strs.Length - 1 - i];
+                strs[strs.Length - 1 - i] = test;
+            }
+        }
+
+        static int ReverseOrder(int[] nums)
+        {
+            int num = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] > nums[i])
+                    {
+                        num++;
+                    }
+                }
+            }
+            
+            return num;
         }
     }
 }
